c43322c65374153425729dfe0472c4916da8f097
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-04-20 22:29:04 +0100

    Made changes to the AddItem test but it's still not passing

--- src/components/AddItem/index.test.js
+++ src/components/AddItem/index.test.js
@@ -7,7 +7,7 @@ import { test, expect } from "@jest/globals";
 // Import stuff from React testing library
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // we need this to use the custom jest-dom matchers
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 
 // Import component that we're testing
 //      AddItem
@@ -17,21 +17,28 @@ import AddItem from "./index.js";
 //- an input with an "Add item:" label
 //- a button containing the value of the `buttonText` prop
 
-test("Input should have 'Add item' label", function () {
+test("Input should have 'Add item' label and button should have 'Add To List' text", () => {
+  // Define the props before rendering the component
+  const addToList = jest.fn(); // This is a mock function that we use to check if the function is called when the button is clicked
+  const buttonText = "Add To List"; // This is the text that we want to check if it appears on the button
+
   // 3 As:
   // Arrange
   //  render the component
-  render(<AddItem />);
+  render(<AddItem addToList={addToList} buttonText={buttonText} />); // This is the component that we want to test and the props that we want to pass to it
 
   // screen.logTestingPlaygroundURL();
   // screen.debug();
 
   // Act
   //  Make a variable to assign the input that we want to check
-  const actual = screen.getByLabelText(/Add item:/i);
-  const expected = "Add item:";
+  const actualLabel = screen.getByLabelText(/Add item:/i);
+  const expectedLabel = "Add item:";
+  const actualButton = screen.getByText(buttonText);
+  const expectedButton = "Add To List"; 
 
   // Assert
   //  Compare actual and expected to check if the label of the input has value of 'AddItem:'
-  expect(actual).toHaveTextContent(expected);
+  expect(actualLabel).toHaveTextContent(expectedLabel);
+  expect(actualButton).toHaveTextContent(expectedButton);
 });
